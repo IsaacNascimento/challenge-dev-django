eb54130462df35caf9ce8826d8e5f66e883afd32
isaac.nascimento <isaacs.nascimento17@gmail.com>
2023-06-21 20:18:24 -0300

    Create super user: admin, password: 123

--- front-charllenge/src/App.js
+++ front-charllenge/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <a href='http://localhost:8000/admin/' target='_blank'>Acesso a Área Admin</a>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload...
